ba45acf241dc2f9143f6e11558d3ca557bbf6586
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-01-28 23:45:27 +0530

    Create begnset4 1.c

---
+++ begnset4 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+int i,l=0;
+clrscr();
+scanf("%[^\n]",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]!=' ')
+l++;
+}
+printf("%d",l);
+getch();
+}

8f0cbb3062d9bc9165dc96a097a8b52c849a6881
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-01-31 18:19:29 +0530

    Create begnset1 9.c

---
+++ begnset1 9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i,a[10],sum=0;
+clrscr();
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+getch();
+}
